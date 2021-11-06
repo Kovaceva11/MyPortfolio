1455325a3eff22a377eb0ac4ed19c7f0e890ed01
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2021-11-05 21:15:36 -0400

    Pushing through the crux

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,28 +1,20 @@
-// Modal Image Gallery
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
-}
-
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("navBar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + "w3-card" + "w3-animate-top" + "w3-white";
+        navbar.className = "ak-bar" + "ak-card" + "ak-animate-top" + "ak-white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace(" ak-card ak-animate-top ak-white", "");
     }
 }
 
 // Used to toggle the menu when you click on the menu hamburger button
 function toggleFunction() {
         var x = document.getElementById("navDrop");
-        if (x.className.indexOf("w3-show") == -1) {
-        x.className += "w3-show";
+        if (x.className.indexOf("ak-show") == -1) {
+        x.className += "ak-show";
         } else {
-        x.className = x.className.replace("w3-show", "");
+        x.className = x.className.replace("ak-show", "");
         }
 }
